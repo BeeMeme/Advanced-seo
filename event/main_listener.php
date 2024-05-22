2e1335fa0927603fe829c3c3753d730e2ad549f2
BeeMeme Official <157950866+BeeMeme@users.noreply.github.com>
2024-05-22 09:51:04 +0530

    Update main_listener.php

--- event/main_listener.php
+++ event/main_listener.php
@@ -1,139 +1,95 @@
 <?php
 /**
- *
- * Advanced Seo. An extension for the phpBB Forum Software package.
- *
- * @copyright (c) 2024, P.J.Borgohain, https://bestpickrs.com/memberlist.php?mode=viewprofile&amp;u=2
+ * Advanced SEO. An extension for the phpBB Forum Software package.
+ * 
+ * @copyright (c) 2024, P.J.Borgohain
  * @license GNU General Public License, version 2 (GPL-2.0)
- *
  */
 
 namespace bestpickrs\advseo\event;
 
-/**
- * @ignore
- */
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use phpbb\event\data;
+use phpbb\language\language;
+use phpbb\controller\helper;
+use phpbb\template\template;
 
-/**
- * Advanced Seo Event listener.
- */
 class main_listener implements EventSubscriberInterface
 {
-	public static function getSubscribedEvents()
-	{
-		return [
-			'core.user_setup'							=> 'load_language_on_setup',
-			'core.page_header'							=> 'add_page_header_link',
-			'core.viewonline_overwrite_location'		=> 'viewonline_page',
-	'core.display_forums_modify_template_vars'	=> 'display_forums_modify_template_vars',
-			'core.permissions'	=> 'add_permissions',
-		];
-	}
+    protected $language;
+    protected $helper;
+    protected $template;
+    protected $php_ext;
 
-	/* @var \phpbb\language\language */
-	protected $language;
+    public function __construct(language $language, helper $helper, template $template, $php_ext)
+    {
+        $this->language = $language;
+        $this->helper = $helper;
+        $this->template = $template;
+        $this->php_ext = $php_ext;
+    }
 
-	/* @var \phpbb\controller\helper */
-	protected $helper;
+    public static function getSubscribedEvents()
+    {
+        return [
+            'core.user_setup'                           => 'load_language_on_setup',
+            'core.page_header'                          => 'add_page_header_link',
+            'core.viewonline_overwrite_location'        => 'viewonline_page',
+            'core.display_forums_modify_template_vars'  => 'display_forums_modify_template_vars',
+            'core.permissions'                          => 'add_permissions',
+        ];
+    }
 
-	/* @var \phpbb\template\template */
-	protected $template;
+    public function load_language_on_setup($event)
+    {
+        $lang_set_ext = $event['lang_set_ext'];
+        $lang_set_ext[] = [
+            'ext_name' => 'bestpickrs/advseo',
+            'lang_set' => 'common',
+        ];
+        $event['lang_set_ext'] = $lang_set_ext;
+    }
 
-	/** @var string phpEx */
-	protected $php_ext;
+    public function add_page_header_link()
+    {
+        $this->template->assign_vars([
+            'U_ADVSEO_PAGE' => $this->helper->route('bestpickrs_advseo_controller', ['name' => 'world']),
+        ]);
+    }
 
-	/**
-	 * Constructor
-	 *
-	 * @param \phpbb\language\language	$language	Language object
-	 * @param \phpbb\controller\helper	$helper		Controller helper object
-	 * @param \phpbb\template\template	$template	Template object
-	 * @param string                    $php_ext    phpEx
-	 */
-	public function __construct(\phpbb\language\language $language, \phpbb\controller\helper $helper, \phpbb\template\template $template, $php_ext)
-	{
-		$this->language = $language;
-		$this->helper   = $helper;
-		$this->template = $template;
-		$this->php_ext  = $php_ext;
-	}
+    public function viewonline_page($event)
+    {
+        if ($event['on_page'][1] === 'app' && strpos($event['row']['session_page'], 'app.' . $this->php_ext . '/demo') === 0)
+        {
+            $event['location'] = $this->language->lang('VIEWING_BESTPICKRS_ADVSEO');
+            $event['location_url'] = $this->helper->route('bestpickrs_advseo_controller', ['name' => 'world']);
+        }
+    }
 
-	/**
-	 * Load common language files during user setup
-	 *
-	 * @param \phpbb\event\data	$event	Event object
-	 */
-	public function load_language_on_setup($event)
-	{
-		$lang_set_ext = $event['lang_set_ext'];
-		$lang_set_ext[] = [
-			'ext_name' => 'bestpickrs/advseo',
-			'lang_set' => 'common',
-		];
-		$event['lang_set_ext'] = $lang_set_ext;
-	}
+    public function display_forums_modify_template_vars($event)
+    {
+        $forum_row = $event['forum_row'];
+        $forum_row['FORUM_NAME'] .= ' ' . $this->language->lang('ADVSEO_EVENT');
+        $event['forum_row'] = $forum_row;
+    }
 
-	/**
-	 * Add a link to the controller in the forum navbar
-	 */
-	public function add_page_header_link()
-	{
-		$this->template->assign_vars([
-			'U_ADVSEO_PAGE'	=> $this->helper->route('bestpickrs_advseo_controller', ['name' => 'world']),
-		]);
-	}
+    public function add_permissions($event)
+    {
+        $permissions = $event['permissions'];
 
-	/**
-	 * Show users viewing Advanced Seo page on the Who Is Online page
-	 *
-	 * @param \phpbb\event\data	$event	Event object
-	 */
-	public function viewonline_page($event)
-	{
-		if ($event['on_page'][1] === 'app' && strrpos($event['row']['session_page'], 'app.' . $this->php_ext . '/demo') === 0)
-		{
-			$event['location'] = $this->language->lang('VIEWING_BESTPICKRS_ADVSEO');
-			$event['location_url'] = $this->helper->route('bestpickrs_advseo_controller', ['name' => 'world']);
-		}
-	}
+        $permissions['a_new_bestpickrs_advseo'] = [
+            'lang' => 'ACL_A_NEW_BESTPICKRS_ADVSEO', 
+            'cat' => 'misc'
+        ];
+        $permissions['m_new_bestpickrs_advseo'] = [
+            'lang' => 'ACL_M_NEW_BESTPICKRS_ADVSEO', 
+            'cat' => 'post_actions'
+        ];
+        $permissions['u_new_bestpickrs_advseo'] = [
+            'lang' => 'ACL_U_NEW_BESTPICKRS_ADVSEO', 
+            'cat' => 'post'
+        ];
 
-	/**
-	 * A sample PHP event
-	 * Modifies the names of the forums on index
-	 *
-	 * @param \phpbb\event\data	$event	Event object
-	 */
-	public function display_forums_modify_template_vars($event)
-	{
-		$forum_row = $event['forum_row'];
-		$forum_row['FORUM_NAME'] .= $this->language->lang('ADVSEO_EVENT');
-		$event['forum_row'] = $forum_row;
-	}
-
-	/**
-	 * Add permissions to the ACP -> Permissions settings page
-	 * This is where permissions are assigned language keys and
-	 * categories (where they will appear in the Permissions table):
-	 * actions|content|forums|misc|permissions|pm|polls|post
-	 * post_actions|posting|profile|settings|topic_actions|user_group
-	 *
-	 * Developers note: To control access to ACP, MCP and UCP modules, you
-	 * must assign your permissions in your module_info.php file. For example,
-	 * to allow only users with the a_new_bestpickrs_advseo permission
-	 * access to your ACP module, you would set this in your acp/main_info.php:
-	 *    'auth' => 'ext_bestpickrs/advseo && acl_a_new_bestpickrs_advseo'
-	 *
-	 * @param \phpbb\event\data	$event	Event object
-	 */
-	public function add_permissions($event)
-	{
-		$permissions = $event['permissions'];
-
-		$permissions['a_new_bestpickrs_advseo'] = ['lang' => 'ACL_A_NEW_BESTPICKRS_ADVSEO', 'cat' => 'misc'];
-		$permissions['m_new_bestpickrs_advseo'] = ['lang' => 'ACL_M_NEW_BESTPICKRS_ADVSEO', 'cat' => 'post_actions'];
-		$permissions['u_new_bestpickrs_advseo'] = ['lang' => 'ACL_U_NEW_BESTPICKRS_ADVSEO', 'cat' => 'post'];
-
-		$event['permissions'] = $permissions;
-	}
+        $event['permissions'] = $permissions;
+    }
 }
