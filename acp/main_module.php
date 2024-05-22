6998bc1bcfb47fa592b8734fea5d36b71a432b51
BeeMeme Official <157950866+BeeMeme@users.noreply.github.com>
2024-05-22 09:56:20 +0530

    Update main_module.php

--- acp/main_module.php
+++ acp/main_module.php
@@ -1,48 +1,48 @@
 <?php
 /**
- *
- * Advanced Seo. An extension for the phpBB Forum Software package.
- *
- * @copyright (c) 2024, P.J.Borgohain, https://bestpickrs.com/memberlist.php?mode=viewprofile&amp;u=2
+ * Advanced SEO. An extension for the phpBB Forum Software package.
+ * 
+ * @copyright (c) 2024
  * @license GNU General Public License, version 2 (GPL-2.0)
- *
  */
 
 namespace bestpickrs\advseo\acp;
 
+use bestpickrs\advseo\controller\acp_controller;
+
 /**
- * Advanced Seo ACP module.
+ * Advanced SEO ACP module.
  */
 class main_module
 {
-	public $page_title;
-	public $tpl_name;
-	public $u_action;
+    public $page_title;
+    public $tpl_name;
+    public $u_action;
 
-	/**
-	 * Main ACP module
-	 *
-	 * @param int    $id   The module ID
-	 * @param string $mode The module mode (for example: manage or settings)
-	 * @throws \Exception
-	 */
-	public function main($id, $mode)
-	{
-		global $phpbb_container;
+    /**
+     * Main ACP module
+     *
+     * @param int $id The module ID
+     * @param string $mode The module mode (e.g., manage or settings)
+     * @throws \Exception
+     */
+    public function main($id, $mode)
+    {
+        global $phpbb_container;
 
-		/** @var \bestpickrs\advseo\controller\acp_controller $acp_controller */
-		$acp_controller = $phpbb_container->get('bestpickrs.advseo.controller.acp');
+        /** @var acp_controller $acp_controller */
+        $acp_controller = $phpbb_container->get('bestpickrs.advseo.controller.acp');
 
-		// Load a template from adm/style for our ACP page
-		$this->tpl_name = 'acp_advseo_body';
+        // Set the template for the ACP page
+        $this->tpl_name = 'acp_advseo_body';
 
-		// Set the page title for our ACP page
-		$this->page_title = 'ACP_ADVSEO_TITLE';
+        // Set the page title for the ACP page
+        $this->page_title = 'ACP_ADVSEO_TITLE';
 
-		// Make the $u_action url available in our ACP controller
-		$acp_controller->set_page_url($this->u_action);
+        // Provide the action URL to the ACP controller
+        $acp_controller->set_page_url($this->u_action);
 
-		// Load the display options handle in our ACP controller
-		$acp_controller->display_options();
-	}
+        // Display the options in the ACP controller
+        $acp_controller->display_options();
+    }
 }
